21a9c7ba8659c24199eabb539ebe6ecb8d15f863
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-01-13 11:04:08 +0530

    initial commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>DemoAdmin</span>
+      <span>© Demo Dev Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
